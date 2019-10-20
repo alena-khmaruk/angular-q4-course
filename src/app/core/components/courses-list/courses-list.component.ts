df613600b4f47af406aefcd766b8a1bb51b9bdaf
alena-khmaruk <elenakhmaruk1@gmail.com>
2019-10-20 16:35:41 +0300

    refactoring: remove initializing from component constructor

--- src/app/core/components/courses-list/courses-list.component.ts
+++ src/app/core/components/courses-list/courses-list.component.ts
@@ -7,31 +7,30 @@ import {Course} from '../course/course.model';
     styleUrls: ['./courses-list.component.scss']
 })
 export class CoursesListComponent implements OnInit {
-    coursesList: Course[];
+    coursesList: Course[] = [
+        {
+            id: 'id_1',
+            title: 'Video 1',
+            creationDate: new Date(),
+            description: 'Description for Video 1',
+            duration: 4
+        },
+        {
+            id: 'id_2',
+            title: 'Video 2',
+            creationDate: new Date(),
+            description: 'Description for Video 2',
+            duration: 4
+        },
+        {
+            id: 'id_3',
+            title: 'Video 3',
+            creationDate: new Date(),
+            description: 'Description for Video 3',
+            duration: 4
+        }];
+
     constructor() {
-        this.coursesList = [
-            {
-                id: 'id_1',
-                title: 'Video 1',
-                creationDate: new Date(),
-                description: 'Description for Video 1',
-                duration: 4
-            },
-            {
-                id: 'id_2',
-                title: 'Video 2',
-                creationDate: new Date(),
-                description: 'Description for Video 2',
-                duration: 4
-            },
-            {
-                id: 'id_3',
-                title: 'Video 3',
-                creationDate: new Date(),
-                description: 'Description for Video 3',
-                duration: 4
-            },
-        ];
     }
 
     ngOnInit() {
